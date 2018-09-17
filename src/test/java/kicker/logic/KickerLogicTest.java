bb9ed074e3a13881bd664f299d460be764dacdcc
Vasiliy_Macsyom <seadlow147@gmail.com>
2018-09-17 06:30:59 +0300

    Small improvement

--- src/test/java/kicker/logic/KickerLogicTest.java
+++ src/test/java/kicker/logic/KickerLogicTest.java
@@ -1,8 +1,11 @@
 package kicker.logic;
 
 import java.awt.*;
+
+import kicker.gui.*;
 import org.junit.*;
 
+import static javafx.scene.input.KeyCode.L;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KickerLogicTest {
@@ -10,12 +13,14 @@ public class KickerLogicTest {
     int[] xOfLines1 = {20, 245, 520, 770};
     int[] numberOfFigures2 = {2, 4, 4, 1};
     int[] xOfLines2 = {125, 380, 655, 880};
+
     final Player player1 = new Player(Color.RED, xOfLines1, numberOfFigures1);
     final Player player2 = new Player(Color.BLUE, xOfLines2, numberOfFigures2);
     final Ball ball = new Ball(player1, player2);
+    final Score score = new Score();
     @Test
-    public void stepTest() {
-
-
+    public void isScoreRight() {
+        score.addCount1();
+        assertEquals(score.getCount1(), 1);
     }
 }
